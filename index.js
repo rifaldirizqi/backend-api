e08348f12fc0ab79dc1991abad32d269684a2bf2
rifaldirizqi <rifaldi.rizqi19@gmail.com>
2018-06-25 14:26:55 +0700

    update

--- index.js
+++ index.js
@@ -3,9 +3,19 @@ const app = express()
 var bodyParser = require('body-parser')
 var jwt = require('jsonwebtoken');
 var secretkey = 'wwwwwwww'
+var mongoclient = require('mongodb').MongoClient
+var ObjectId = require('mongodb').ObjectId
+const logger = require('morgan')
 
 //app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json())
+app.use(logger('dev'))
+
+var db = null
+mongoclient.connect("mongodb://localhost:27017/apidb", (err, client) => {
+    if (err) throw err
+    db = client.db('apidb')
+})
 
 app.get('/', (req, res) => {
     let obj = {
@@ -15,26 +25,109 @@ app.get('/', (req, res) => {
     res.status(404).end('Tidak ditemukan')
 })
 
-app.post('/login', (req, res) => {
-    let body = req.body
-    if(body.username == 'wow' && body.password == '1234'){
-        let token = jwt.sign({'Hello' : 'World'}, secretkey)
-        res.send(token)
-    }else{
-        res.status(403).end('Forbidden')
+app.post('/register', (req, res) => {
+    let datadb = {
+        'username' : req.body.username,
+        'password' : req.body.password
     }
-
+    db.collection('users').insertOne(datadb)
+    .then ((result) => {
+        res.json(result)
+    })
 })
 
-app.get('/profile', (req, res) => {
+app.post('/login', (req, res) => {
+    db.collection('users').findOne({
+        'username' : req.body.username,
+        'password' : req.body.password  
+    })
+    .then((result) => {
+        if (result == null){
+            res.status(404).end('Username/Password Salah')
+        }else{
+            let token = jwt.sign(result, secretkey)
+            let responsedata = {
+                token : token,
+                userdata : result
+            }
+            res.json(responsedata)
+        }
+    })
+})
+
+app.put('/update', (req, res) => {
     var token = req.headers['authorization']
     jwt.verify(token, secretkey, function(err, decoded) {
         if(decoded == undefined){
-            res.status(403).end('Forbidden')
+            res.status(403).end('Token Salah')
         }else{
-            res.status(200).end('OK')
+            let userid = decoded._id
+            db.collection('users').findOne({
+                '_id' : ObjectId(userid)
+            })
+            .then((result) => {
+                if (result == null){
+                    res.status(404).end('Username/Password Salah')
+                }else{
+                    db.collection('users').updateOne(
+                        {'_id' : ObjectId(userid)},
+                        {$set : {'username' : req.body.username}}
+                    )
+                    .then ((resultupdate) => {
+                        res.json(resultupdate)
+                    })
+                }               
+            })
         }
-    });
+    })
+})
+
+
+app.get('/myprofile', (req, res) => {
+    var token = req.headers['authorization']
+    jwt.verify(token, secretkey, function(err, decoded) {
+        if(decoded == undefined){
+            res.status(403).end('Token Salah')
+        }else{
+            let userid = decoded._id
+            db.collection('users').findOne({
+                '_id' : ObjectId(userid)
+            })
+            .then((result) => {
+                if (result == null){
+                    res.status(404).end('Username/Password Salah')
+                }else{
+                    res.json(result)
+                }               
+            })
+        }
+    })
+})
+
+app.delete('/delete', (req, res) => {
+    var token = req.headers['authorization']
+    jwt.verify(token, secretkey, function(err, decoded) {
+        if(decoded == undefined){
+            res.status(403).end('Token Salah')
+        }else{
+            let userid = decoded._id
+            db.collection('users').findOne({
+                '_id' : ObjectId(userid)
+            })
+            .then((result) => {
+                if (result == null){
+                    res.status(404).end('Username/Password Salah')
+                }else{
+                    db.collection('users').deleteOne(
+                        {'_id' : ObjectId(userid)}
+                    )
+                    .then ((resultdelete) => {
+                        res.json(resultdelete)
+                    })
+                }               
+            })
+        }
+    })
 })
 
 app.get('/myprofile/:userid', (req, res) => {
